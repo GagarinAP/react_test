eb5f8cfbd53eb4e9b9ba5512c5577a31ab4b62be
GagarinAP <gagarin.web@gmail.com>
2018-07-08 12:04:44 +0300

    Mod 4 redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { onChangeData } from './HomeReducer';
 
-const Home = ({ match, location, history }) => {
-		return (
-				<div>
-								Home
-				</div>
-		);
-};
+const Home = ({ match, location, history, data, onChangeData }) => (
+		<div>
+				<h1>Im from state in home <strong>{data}</strong></h1>
+				<input type="text" onChange={e => onChangeData(e.target.value)}/>
+		</div>
+);
 
-export default withRouter(Home);
+const mapStateToProps = (state) => ({
+		data: state.home.data
+});
+
+const mapDispatchToProps = { onChangeData };
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Home));
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,27 @@
+import React from 'react';
+import {
+		BrowserRouter as Router,
+		Switch,
+		Route
+} from 'react-router-dom';
+
 import Home from './Home';
 import About from './About';
 import { Profile, ProfileRandom, Search } from './Profile';
 import Layout from './Layout';
 
-export {
-		Home,
-		About,
-		Profile,
-		ProfileRandom,
-		Search,
-		Layout
-}
+const App = () => (
+		<Router>
+				<Switch>
+						<Layout>
+								<Route exact path='/' component={Home}/>
+								<Route path='/about' component={About}/>
+								<Route exact path='/profile' component={Profile}/>
+								<Route path='/profile/:id' component={ProfileRandom}/>
+								<Route path='/search' component={Search}/>
+						</Layout>
+				</Switch>
+		</Router>
+);
+
+export default App;
